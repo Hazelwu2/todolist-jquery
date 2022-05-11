5b95db5642364fb6f618639e662b605251a18100
Hazel Wu <tinazx056@gmail.com>
2022-05-12 02:40:17 +0800

    feat: 刪除待辦

--- index.js
+++ index.js
@@ -14,19 +14,37 @@ const appendItem = () => {
   </li>`)
 }
 
+const checkForm = () => {
+  const value = $('#todo').val()
+  if (!value) {
+    alert('請填寫資料')
+    return false
+  }
+
+  return true
+}
 // 新增待辦
 const addNewTodo = () => {
+  if (!checkForm()) return
 
   appendItem()
   clearInput()
 }
 
+// 刪除待辦
+const deleteTodo = (e) => {
+  $(e.target).parent().closest('li').remove()
+}
+
 
 // 監聽
-$(function () {
-  $('.delete').each(function () {
+$(() => {
+  $('.todolist__item').on('click', '.delete', (e) => deleteTodo(e))
+
+  $('.todolist__tabs li').each(function () {
     $(this).click(function () {
-      $(this).closest('li').remove()
+      $(this).siblings().find('a').removeClass('active')
+      $(this).find('a').addClass('active')
     })
   })
 })
